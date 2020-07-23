5be52d5189cfa3da56856945f750a455b82bbe77
root <root@ip-172-31-34-126.ap-south-1.compute.internal>
2020-07-23 08:52:47 +0000

    first commit

---
+++ karma.conf.js
@@ -0,0 +1,48 @@
+
+// Karma configuration file, see link for more information
+// https://karma-runner.github.io/0.13/config/configuration-file.html
+process.env.CHROME_BIN = require('puppeteer').executablePath()
+
+module.exports = function (config) {
+  config.set({
+    basePath: '',
+    frameworks: ['jasmine', '@angular/cli'],
+    browsers: ['ChromeHeadless'],
+    plugins: [
+      require('karma-jasmine'),
+      require('karma-chrome-launcher'),
+      require('karma-jasmine-html-reporter'),
+      require('karma-coverage-istanbul-reporter'),
+      require('@angular/cli/plugins/karma'),
+      require('karma-phantomjs-launcher'),
+      require('karma-coverage'),
+    ],
+    client:{
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
+    coverageIstanbulReporter: {
+      reports: [ 'html', 'lcovonly','text-summary' ],
+      fixWebpackSourcePaths: true
+    },
+    angularCli: {
+      environment: 'dev'
+    },
+    reporters: ['progress', 'kjhtml'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    customLaunchers: {
+        ChromeHeadless: {
+            base: 'Chrome',
+            flags: [
+            '--headless',
+            '--disable-web-security',
+            '--disable-setuid-sandbox',
+            '--no-sandbox',
+            '--remote-debugging-port=9222',]
+      }
+},
+    singleRun: true
+  });
+};
